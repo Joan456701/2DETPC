b97a1418a2def2cac9036657429c8681fcdc7168
Joan Perera Torres <456701.clot@fje.edu>
2025-02-22 17:54:28 +0100

    Ultimos Cambios

--- Assets/Scripts/Environment/CherryController.cs
+++ Assets/Scripts/Environment/CherryController.cs
@@ -1,19 +1,38 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 public class CherryController : MonoBehaviour
 {
+    public SpriteRenderer GemRender;
+    public float Distance = 1.5f;
+    public float Distance2 = 1.5f;
+    public float Speed = 1f;
+    public Ease easing;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GemMove();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void GemMove()
+    {
+        transform.DOLocalMoveX(Distance, Speed).SetEase(easing).OnComplete(() =>
+        { 
+            transform.DOLocalMoveX(Distance2, Speed).SetEase(easing).OnComplete(() =>
+            {
+                GemMove();
+            });
+        });
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
